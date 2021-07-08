4074fa63a049b0fb92feda6557776f3bd81b5a76
Fábio Sano <fabinhosano@hotmail.com>
2021-07-08 14:52:24 -0300

    i

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('imagem')
 <div class=" col-xl-7 col-lg-6 col-md-12 text-lg-end text-center">
-<img src="{{asset('img/cachorro.png')}}"alt="" class="img-fluid"  >
+<img src="{{ asset('assets/img/cachorro.png') }}"alt="" class="img-fluid"  >
 </div>
 @endsection
 
