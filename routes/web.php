951c355f123777b34a097c03d040a2db1f4f9457
Fábio Sano <fabinhosano@hotmail.com>
2021-07-08 11:28:58 -0300

    auth

--- routes/web.php
+++ routes/web.php
@@ -53,10 +53,3 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
