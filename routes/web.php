33d470ddc0b6efac2959cb38c4ddb93ca80a4feb
Fábio Sano <fabinhosano@hotmail.com>
2021-07-08 11:03:06 -0300

    v5

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,7 @@ Route::get('/sair', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
